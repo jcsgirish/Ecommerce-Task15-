ce27747b3a1b4f9d0de34cd6fe88a9a46905fb8d
jcsgirish <jcsgirish7@gmail.com>
2023-05-15 23:44:06 +0530

    Addressing security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './Store/Context';
 
 function App() {
+  const ctx = useContext(AuthContext);
+  
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
+    <Switch>
+      <Route path='/' exact>
+        <HomePage />
+      </Route>
+      {!ctx.isLoggedIn && <Route path='/auth'>
+        <AuthPage />
+      </Route>}
+       <Route path='/profile'>
+        {ctx.isLoggedIn && <UserProfile />}
+        {!ctx.isLoggedIn && <Redirect to="/auth" />}
+      </Route>
+      <Route path="*" exact>
+      <Redirect to="/" />
+      </Route>
+    </Switch>
+  </Layout>
   );
 }
 
